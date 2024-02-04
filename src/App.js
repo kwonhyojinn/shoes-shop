f3141fd5804c34ebba7d8542341742f92b97ab09
kwonhyojinn <kwonhyojinn@gmail.com>
2024-02-04 15:35:16 +0900

    add detail page and react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import data from "./data.js";
 import { useState } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import DetailPage from "./pages/DetailPage.jsx";
 
 function App() {
   let [shoes] = useState(data);
@@ -17,17 +19,28 @@ function App() {
             <Nav.Link href="#new">New</Nav.Link>
             <Nav.Link href="#best">Best</Nav.Link>
           </Nav>
+          <Link to="/">home</Link>
         </Container>
       </Navbar>
 
-      <div className="main-bg"></div>
-      <div className="container">
-        <div className="row">
-          {shoes.map((a, i) => {
-            return <Card shoes={shoes[i]} i={i} />;
-          })}
-        </div>
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map((a, i) => {
+                    return <Card shoes={shoes[i]} i={i} />;
+                  })}
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route path="/detail" element={<DetailPage />} />
+      </Routes>
     </div>
   );
 }
@@ -35,10 +48,12 @@ function App() {
 function Card(props) {
   return (
     <div className="col-md-4">
-      <img
-        src={process.env.PUBLIC_URL + `/shoes${props.i + 1}.jpeg`}
-        width="80%"
-      />
+      <Link to="/detail">
+        <img
+          src={process.env.PUBLIC_URL + `/shoes${props.i + 1}.jpeg`}
+          width="80%"
+        />
+      </Link>
       <h5>{props.shoes.title}</h5>
       <p>{props.shoes.price}원</p>
     </div>
