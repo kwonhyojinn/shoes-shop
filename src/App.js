2d6084631c838832da039395361260b934cccbe7
kwonhyojinn <kwonhyojinn@gmail.com>
2024-02-04 15:08:41 +0900

    add card component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import data from "./data.js";
+import { useState } from "react";
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="light" data-bs-theme="light">
@@ -15,6 +19,28 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+
+      <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={process.env.PUBLIC_URL + `/shoes${props.i + 1}.jpeg`}
+        width="80%"
+      />
+      <h5>{props.shoes.title}</h5>
+      <p>{props.shoes.price}원</p>
     </div>
   );
 }
