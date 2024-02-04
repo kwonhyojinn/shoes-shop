8784d26a67720df0d98df68765f4ab8bd3a255d4
kwonhyojinn <kwonhyojinn@gmail.com>
2024-02-04 14:32:11 +0900

    add bootstrap package and Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Shoes Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#all">All</Nav.Link>
+            <Nav.Link href="#new">New</Nav.Link>
+            <Nav.Link href="#best">Best</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
